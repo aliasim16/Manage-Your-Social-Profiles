7649780b676036eeaca25f76c60eb4cbf0de8531
Ali Asım Bulut <aliasim1625@hotmail.com>
2023-08-13 16:17:34 +0300

    Dark light mode css özellikleri güncellendi

--- src/components/Our_Services.jsx
+++ src/components/Our_Services.jsx
@@ -30,7 +30,7 @@ function Our_services(props) {
             <div className="bottom">
               <div className="left_div">
                 <button type="button">
-                  <div id="blue_box"></div>
+                  <div id=" "></div>
                   Read More
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
                     <path d="M6.5 12.5H17.5M17.5 12.5L12.9118 8.5M17.5 12.5L12.9118 16.5" stroke="#2A3126" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
